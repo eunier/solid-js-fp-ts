a481f8ca8bdf8ab1df81dc22a3dcde11b226aa0f
Yunier Alvarez <yunieralvarez@gmail.com>
2022-11-13 08:37:38 -0500

    option

--- src/Helper.ts
+++ src/Helper.ts
@@ -1,17 +1,36 @@
+import { flow } from 'fp-ts/function';
+import * as O from 'fp-ts/Option';
 import * as Types from 'Helper.type';
 import { JSX } from 'solid-js';
 
 export const getEvent = <T, E extends Event>(
   event: Parameters<JSX.EventHandler<T, E>>[0]
-): Types.HelperEvent => {
+): O.Option<Types.HelperEvent> => {
   switch (event.type) {
     case 'input':
-      return {
+      return O.some({
         _tag: 'InputElementInputEvent',
         event: event as unknown as Types.InputElementInputEvent['event'],
-      };
+      });
 
     default:
-      return { _tag: 'Event', event };
+      return O.some({ _tag: 'Event', event });
   }
 };
+
+export const eventToCurrentTargetValue = (
+  event: Types.HelperEvent
+): O.Option<string> => {
+  switch (event._tag) {
+    case 'InputElementInputEvent':
+      return O.some(event.event.currentTarget.value);
+
+    default:
+      return O.none;
+  }
+};
+
+export const pickCurrentTargetValue = flow(
+  getEvent,
+  O.chain(eventToCurrentTargetValue)
+);
